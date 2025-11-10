c787b7b4ceccfdc34b3a8e88a17810ef67695d81
choyoungseo20 <whdudtj0809@naver.com>
2025-11-10 18:10:07 +0900

    first commit

---
+++ src/main/java/com/realtimeeditor/RealtimeeditorApplication.java
@@ -0,0 +1,13 @@
+package com.realtimeeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeeditorApplication.class, args);
+	}
+
+}
