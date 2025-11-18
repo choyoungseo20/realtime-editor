c5950f71741397ae99b274f2eeb310647be68bb8
choyoungseo20 <whdudtj0809@naver.com>
2025-11-18 14:23:51 +0900

    [FE] refactor: 회원가입 기능 추가에 따른 버튼 클릭 action 분기 처리

--- frontend/src/main/java/com/realtimeeditor/controller/LoginController.java
+++ frontend/src/main/java/com/realtimeeditor/controller/LoginController.java
@@ -2,7 +2,9 @@ package com.realtimeeditor.controller;
 
 import com.realtimeeditor.model.LoginInfo;
 import com.realtimeeditor.service.LoginService;
+import com.realtimeeditor.service.SignUpService;
 import com.realtimeeditor.view.LoginView;
+import com.realtimeeditor.view.SignUpView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
@@ -16,16 +18,35 @@ public class LoginController implements ActionListener {
         this.loginView = loginView;
         this.loginService = loginService;
         loginView.setVisible(true);
+        loginView.setNavigateSignUpActionListener(this);
         loginView.setLoginActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        if (source == loginView.getSignUpButton()) {
+            navigateToSingUp();
+        } else if (source == loginView.getLoginButton()) {
+            handleLogin();
+        }
+    }
+
+    private void navigateToSingUp() {
+        SignUpView signUpView = new SignUpView();
+        SignUpService signUpService = new SignUpService();
+        SignUpController signUpController = new SignUpController(signUpView, signUpService);
+        loginView.dispose();
+    }
+
+    private void handleLogin() {
         String nickname = loginView.getNickname();
         String password = loginView.getPassword();
-        LoginInfo loginInfo = new LoginInfo(nickname, password);
-        boolean success = loginService.login(loginInfo);
 
+        LoginInfo loginInfo = new LoginInfo(nickname, password);
+
+        boolean success = loginService.login(loginInfo);
         if (success) {
             JOptionPane.showMessageDialog(loginView, "로그인 성공");
         } else {
