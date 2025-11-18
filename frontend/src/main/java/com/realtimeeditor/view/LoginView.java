88d8dfeb8672aaf78c3b4ee554f97c03d32b22b9
choyoungseo20 <whdudtj0809@naver.com>
2025-11-18 14:23:35 +0900

    [FE] refactor: 클래스의 이름 변경에 따른 필드명 수정

--- frontend/src/main/java/com/realtimeeditor/view/LoginView.java
+++ frontend/src/main/java/com/realtimeeditor/view/LoginView.java
@@ -1,25 +1,38 @@
 package com.realtimeeditor.view;
 
-import com.realtimeeditor.view.panel.LoginButtonPanel;
+import com.realtimeeditor.view.panel.AuthButtonPanel;
 import com.realtimeeditor.view.panel.LoginInputPanel;
-import com.realtimeeditor.view.panel.ServiceNamePanel;
+import com.realtimeeditor.view.panel.ServiceHeaderPanel;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class LoginView extends JFrame {
 
-    private ServiceNamePanel serviceNamePanel;
+    private ServiceHeaderPanel serviceHeaderPanel;
     private LoginInputPanel loginInputPanel;
-    private LoginButtonPanel loginButtonPanel;
+    private AuthButtonPanel authButtonPanel;
 
     public LoginView() {
         initWindow();
         initComponents();
     }
 
+    public void setNavigateSignUpActionListener(ActionListener actionListener) {
+        authButtonPanel.setNavigateSignUpActionListener(actionListener);
+    }
+
     public void setLoginActionListener(ActionListener actionListener) {
-        loginButtonPanel.setLoginActionListener(actionListener);
+        authButtonPanel.setLoginActionListener(actionListener);
+    }
+
+    public JButton getSignUpButton() {
+        return authButtonPanel.getNavigateSignUpButton();
+    }
+
+    public JButton getLoginButton() {
+        return authButtonPanel.getLoginButton();
     }
 
     public String getNickname() {
@@ -40,13 +53,13 @@ public class LoginView extends JFrame {
     }
 
     private void initComponents() {
-        serviceNamePanel = new ServiceNamePanel();
-        add(serviceNamePanel, BorderLayout.NORTH);
+        serviceHeaderPanel = new ServiceHeaderPanel();
+        add(serviceHeaderPanel, BorderLayout.NORTH);
 
         loginInputPanel = new LoginInputPanel();
         add(loginInputPanel, BorderLayout.CENTER);
 
-        loginButtonPanel = new LoginButtonPanel();
-        add(loginButtonPanel, BorderLayout.SOUTH);
+        authButtonPanel = new AuthButtonPanel();
+        add(authButtonPanel, BorderLayout.SOUTH);
     }
 }
