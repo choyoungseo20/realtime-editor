8d3ef9230619675a81f59f60c17369b75e90b8e5
choyoungseo20 <whdudtj0809@naver.com>
2025-11-24 18:15:28 +0900

    [BE] feat: 사용자 예외 처리 메시지를 묶은 class 생성

---
+++ backend/src/main/java/com/realtimeeditor/exception/UserExceptionMessage.java
@@ -0,0 +1,6 @@
+package com.realtimeeditor.exception;
+
+public class UserExceptionMessage {
+    public static final String USER_NOT_FOUND = "존재하지 않는 사용자입니다.";
+    public static final String PASSWORD_MISMATCH = "비밀번호가 일치하지 않습니다.";
+}
